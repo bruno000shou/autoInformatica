088b081e42b3257a5844c5b53d8318fcd1ffa45a
Bruno Sercundo <bruno000shou@gmail.com>
2025-10-20 19:17:41 -0300

    estrutura inicial do programa

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const { engine } = require ('express-handlebars');
 
 //importando css e bootstrap para o app
-app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
-app.use('/css', express.static('./css'));
+app.use('/bootstrap', express.static(__dirname+'/node_modules/bootstrap/dist'));
+app.use('/css', express.static(__dirname+'/css'));
 
 //indicando que usaremos json nesse app
 app.use(express.json());
@@ -15,6 +15,30 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+//rotas
+app.get('/ordemServico', (req,res) => {
+    res.render('paginaOrdemServico')
+})
+
+app.get('/caixa', (req,res) => {
+    res.render('paginaCaixa')
+})
+
+app.get('/recibo', (req,res) => {
+    res.render('paginaRecibo')
+})
+
+app.get('/cliente', (req,res) => {
+    res.render('paginaCliente')
+})
+
+app.get('/agenda', (req,res) => {
+    res.render('paginaAgenda')
+})
+
+app.get('/produto', (req,res) => {
+    res.render('paginaProduto')
+})
 
 app.get('/', (req,res) => {
     res.render('mainPage')
